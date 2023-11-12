7d8d5efa675d39e30b19657d0585928c608c746c
Arthistic04 <arthurgulane@gmail.com>
2023-11-12 23:10:32 +0800

    Initial commit

---
+++ SeismicWeb5/Controllers/HomeController.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SeismicWeb5.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Firebase.Database;
+using Firebase.Database.Query;
+using FirebaseAdmin.Auth;
+
+
+namespace SeismicWeb5.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly FirebaseClient firebaseClient;
+
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger, FirebaseClient firebaseClient)
+        {
+            _logger = logger;
+            this.firebaseClient = firebaseClient;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Earthquake()
+        {
+            return View();
+        }
+
+        public IActionResult Monitor()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> GetDataFromFirebase()
+        {
+            try
+            {
+                var data = await FetchDataFromFirebaseAsync();
+                return Json(data);
+            }
+            catch (Exception ex)
+            {
+                // Log the exception
+                Console.Error.WriteLine($"Error fetching data from Firebase: {ex.Message}");
+                return StatusCode(500, "Internal Server Error");
+            }
+        }
+
+        private async Task<List<DataModel>> FetchDataFromFirebaseAsync()
+        {
+            var dataSnapshot = await firebaseClient.Child("User001").OnceAsync<DataModel>();
+
+            var data = new List<DataModel>();
+
+            foreach (var dataSnap in dataSnapshot)
+            {
+                var timestamp = dataSnap.Object.Timestamp;
+                var x = dataSnap.Object.gyroX;
+                var y = dataSnap.Object.gyroY;
+                var z = dataSnap.Object.gyroZ;
+
+                data.Add(new DataModel { Timestamp = timestamp, gyroX = x, gyroY = y, gyroZ = z });
+            }
+
+            return data;
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+    public class DataModel
+    {
+        public DateTime Timestamp { get; set; }
+        public float gyroX { get; set; }
+        public float gyroY { get; set; }
+        public float gyroZ { get; set; }
+    }
+}
---
+++ SeismicWeb5/Startup.cs
@@ -0,0 +1,74 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using FirebaseAdmin;
+using Firebase.Database;
+using Google.Apis.Auth.OAuth2;
+
+namespace SeismicWeb5
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var firebaseCredentialsPath = "C:\\Users\\Arthur\\source\\repos\\SeismicWeb5\\SeismicWeb5\\wwwroot\\lib\\firebasekey.json";
+            FirebaseApp.Create(new AppOptions
+            {
+                Credential = GoogleCredential.FromFile(firebaseCredentialsPath),
+            });
+
+            services.AddScoped<FirebaseClient>(_ =>
+            {
+                var firebaseUrl = "https://seismoweb-9e2a6-default-rtdb.asia-southeast1.firebasedatabase.app"; // Replace with your Firebase Realtime Database URL
+                return new FirebaseClient(firebaseUrl);
+            });
+
+            services.AddMvc();
+            services.AddControllersWithViews();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
